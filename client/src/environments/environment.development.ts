e6c1e368331bee54db24611dd820ee6dd6bc7c7a
morriss <lim.shawnmichael@gmail.com>
2024-12-03 00:03:22 +0800

    section 16 stripe

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51QP5JIERqv796dJziaOkWdbMt8AyzzWGuwPphDJLhNIBzfK3EZ4G19mRaornj9LVvozCRdpiLBnutQmibFYl3tmV005mYg6UZa',
 };
